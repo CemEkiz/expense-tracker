ea9797c761b9331c5bbc0b3fb7417953c78cf2e6
Cem Ekiz <ekizcem.pro@gmail.com>
2023-04-05 19:55:14 +0200

    33: writing more complex jsx code

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,5 +1,13 @@
 const ExpenseItem = () => {
-  return <h1>ExpenseItem</h1>;
+  return (
+    <div>
+      <div>March 28th 2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.67</div>
+      </div>
+    </div>
+  );
 };
 
 export default ExpenseItem;
